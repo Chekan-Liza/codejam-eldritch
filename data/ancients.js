9dcdb9e2cd9f54cb4921478e0f709bd79a5aa23d
Chekan-Liza <70697985+Chekan-Liza@users.noreply.github.com>
2022-08-27 10:48:36 +0300

    add data

---
+++ data/ancients.js
@@ -0,0 +1,90 @@
+//mport Ancients from '../assets/Ancients/index'
+
+const ancientsData = [
+  {
+    id: 'azathoth',
+    name: 'Azathoth',
+    ru_name: 'Азатот',
+    //cardFace: Ancients.azathoth,
+    firstStage: {
+      greenCards: 1,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 2,
+      blueCards: 1,
+      brownCards: 3,
+    },
+    thirdStage: {
+      greenCards: 2,
+      blueCards: 0,
+      brownCards: 4,
+    },
+  },
+  {
+    id: 'cthulhu',
+    name: 'Cthulhu',
+    ru_name: 'Ктулху',
+    //cardFace: Ancients.cthulhu,
+    firstStage: {
+      greenCards: 0,
+      blueCards: 2,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 1,
+      blueCards: 0,
+      brownCards: 3,
+    },
+    thirdStage: {
+      greenCards: 3,
+      blueCards: 0,
+      brownCards: 4,
+    },
+  },
+  {
+    id: 'iogSothoth',
+    name: 'IogSothoth',
+    ru_name: 'Йог-Сотот',
+    //cardFace: Ancients.iogSothoth,
+    firstStage: {
+      greenCards: 0,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 2,
+      blueCards: 1,
+      brownCards: 3,
+    },
+    thirdStage: {
+      greenCards: 3,
+      blueCards: 0,
+      brownCards: 4,
+    },
+  },
+  {
+    id: 'shubNiggurath',
+    name: 'ShubNiggurath',
+    ru_name: 'Шуб-Ниггурат',
+    //cardFace: Ancients.shubNiggurath,
+    firstStage: {
+      greenCards: 1,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 3,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    thirdStage: {
+      greenCards: 2,
+      blueCards: 0,
+      brownCards: 4,
+    },
+  },
+]
+
+export default ancientsData
